b2c61cba732163eda726355d961d74b8494a2890
Aliot <aliot@tut.by>
2015-09-10 16:54:44 +0300

    laba1

--- users.php
+++ users.php
@@ -12,16 +12,15 @@ $user1 = new User();
 $user1->name = 'Ivan';
 $user1->login = 'ivan';
 $user1->password = 123;
+$user1->showInfo();
 $user2 = new User();
 $user2->name = 'Petr';
 $user2->login = 'petr';
 $user2->password = 456;
+$user2->showInfo();
 $user3 = new User();
 $user3->name = 'John';
 $user3->login = 'john';
 $user3->password = 789;
-$user1->showInfo();
-$user2->showInfo();
 $user3->showInfo();
-
 ?>
