e811458fce37a23054f5916162914eaa2e4527d0
Harnet69 <motobycom@tut.by>
2015-09-10 16:14:11 +0300

    add new to Shpora & add new class into index

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <h1>Работа с OOP по версии Harnet</h1>
 <?
 	class Animal {
-		//
+		public $name;
+		public $lastname;
+		
+		function __construct($n,$l){
+			$this->name = $n;
+			$this->lastname = $l;
+		}
+		
+		function show_anim(){
+			echo $this->name."<br>";
+			echo $this->lastname."<br>";
+		}
 	}
 
-	$cat = new Animal;
-	$dog = new Animal();
-	echo gettype ($cat);
+	$cat = new Animal("Атос","Атосыч");
+	$cat->show_anim();
+	
+	
+	
 ?> 
