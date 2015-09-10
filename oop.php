aab1597c3cad33267165a2b06cf568c44f8f2e41
Aliot <aliot@tut.by>
2015-09-10 20:41:01 +0300

    clone user

--- oop.php
+++ oop.php
@@ -2,22 +2,31 @@
 class Animal{
 	public $name;
 	public $age = 0;
+	
 	function sayHello($word){
 		echo $this->name.' say '.$word;
 		$this->DrawBr();
 	}
+	
 	function DrawBr(){
 		echo '<br>';
 	}
+	
 	function __construct($num){
 		echo 'Object '.$num.' created <br>';
 	}
+	
 	function __destruct(){
 		echo "Object deleted <br>";
+	}
+	
+	function __clone(){
+		echo 'Object cloned <br>';
 	}
 }
 $cat = new Animal(1);
 $dog = new Animal(2);
+$supercat = clone $cat;
 $cat->name = "Cat";
 $dog->name = "Dog";
 $cat->sayHello("Myau");
--- users.php
+++ users.php
@@ -15,13 +15,21 @@ class User{
 	}
 		
 	function __destruct(){
-		echo '<p>User - '.$this->login.' deleted';
+		echo '<br>User - '.$this->login.' deleted';
+	}
+	
+	function __clone(){
+		$this->name = 'Guest';
+		$this->login = 'guest';
+		$this->password = '111';
 	}
 }
-$user1 = new User('Ivan','ivan',123);
+$user1 = new User('Ivan','ivan','123');
 $user1->showInfo();
-$user2 = new User('Petr','petr',456);
+$user2 = new User('Petr','petr','456');
 $user2->showInfo();
-$user3 = new User('John','john',789);
+$user3 = new User('John','john','789');
 $user3->showInfo();
+$user4 = clone $user3;
+$user4->showInfo();
 ?>
