aedf432dfc6a044b097c7da72c086d2b3ce1eb81
Harnet <motobycom@tut.by>
2015-09-12 17:41:25 +0300

    Add user Yasha in Myuser

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <h1>Работа с OOP по версии Harnet</h1>
 <?
-class User {
+class User 
+{
 	public $name;
 	public $login;
 	public $password;
 	
-	function __construct ($n, $l, $p){
+	function __construct ($n, $l, $p)
+	{
 		$this->name = $n;
 		$this->login = $l;
 		$this->password = $p;
@@ -13,28 +15,45 @@ class User {
 		echo "Эта сущность принадлежит классу ".__class__."<br>";
 	}
 		
-	function __clone(){
-		echo "Объект ".$this->name." Клонирован<br>";		
+	function __clone()
+	{
+		echo "Объект ".$this->name." Клонирован<br>";
+		$this->name =  "Клонированный ". $this->name;
 	}
 	
 	
-	function __destruct(){
+	function __destruct()
+	{
 		echo "Объект ".$this->name." удалён<br>";		
 	}
 
-	function showInfo(){
+	function showInfo()
+    {
 		echo "Имя пользователя : ".$this->name.", логин : ".$this->login.", пароль : ".$this->password."<br>";
-		echo "Использованный метод ".__METHOD__."<hr>";
+		echo "Использованный метод ".__METHOD__."<br>";
 	}
 }
 
-$user1 = new User("Иван","ivan","12345");
+class MyUser extends User
+{
+	public $age;
+	function ShowAge ()
+	{
+	echo "Возраст : ".$this->age."<hr>";
+	}
+}
+
+/*$user1 = new User("Иван","ivan","12345");
 $user1->showInfo();
 $myuser = clone $user1;
-//$myuser->showInfo();
+$myuser->showInfo();
 $user2 = new User("Василий","vasil","34567");
 $user2->showInfo();
 $user3 = new User("Пётр","petr","98765");
 $user3->showInfo();
-echo "Код закончился, можно приступать к удалению<hr>";
+echo "Код закончился, можно приступать к удалению<hr>"; */
+$myuser1 = new MyUser("Яша","Yasha","11111");
+$myuser1->showInfo();
+$myuser1->age = 25;
+$myuser1->ShowAge();
 ?> 
