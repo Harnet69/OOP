42d3366466531b4ff45d86dd22ebce7f82d3bd6c
Aliot <aliot@tut.by>
2015-09-17 12:40:31 +0300

    end try-catch

--- try.php
+++ try.php
@@ -3,11 +3,13 @@ try{
 	$a = 1;
 	$b = 0;
 	if($b == 0){
-		throw new Exception("Error!");}
+		throw new Exception("Error!");
+		}
 	$c = $a/$b;
 }catch(Exception $e){
-	echo $e->getMesssage();
-	echo $e->getLine();
-	echo $e->getFile();
+	echo 'Error: '.$e->getMessage().'<br>';
+	echo 'Line #: '.$e->getLine().'<br>';
+	echo 'File: '.$e->getFile().'<br>';
 }
+echo 'The work continues!';
 ?>
