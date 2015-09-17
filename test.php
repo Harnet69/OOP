4a54c9c35384c2d93ff471546a7ef995e4e0bf14
Harnet69 <motobycom@tut.by>
2015-09-17 16:42:38 +0300

    my day

--- test.php
+++ test.php
@@ -1,3 +1,24 @@
 <?
+abstract class Human {
+	public $fio;
+	public $age;
+	function __construct($f, $a){
+		$this->fio = $f;
+		$this->age = $a;
+	}
+	function Show(){
+		echo $this->fio." ".$this->age;
+	}
+	abstract function add();
+}
+class Man extends Human{
+	function add(){
+		echo "Lelele";
+	}
+	
+}
 
+$human1 = new Man("Иванов Иван Иваныч", 55);
+$human1->Show();
+$human1->add();
 ?>
