bcec59a4fb2a6343c9b2bc0cbeeea79abda50495
Aliot <aliot@tut.by>
2015-09-25 18:31:04 +0300

    modified:   help_sqlite.txt

--- NewsDB.class.php
+++ NewsDB.class.php
@@ -7,7 +7,19 @@ class NewsDB implements INewsDB{
 	const DB_NAME = 'Z:\home\oop\www\news.db';
 	
 	function __construct(){
-		$this->_db = new SQLite3(self::DB_NAME);
+		if(is_file(self::DB_NAME)){
+			$this->_db = new SQLite3(self::DB_NAME);
+		}else{
+			$this->_db = new SQLite3(self::DB_NAME);
+			$sql = "CREATE TABLE msgs(
+						id INTEGER PRIMARY KEY AUTOINCREMENT,
+						title TEXT,
+						category INTEGER,
+						description TEXT,
+						source TEXT,
+						datetime INTEGER)";
+			$this ->_db -> exec($sql) or die($this ->_db);
+		}
 	}
 	function __destruct(){
 		unset($this->_db);
